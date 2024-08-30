3b5a01155827a6d2b4b61347003550eea792c3cc
Shavar Dawkins <shavardawkins@gmail.com>
2024-08-29 17:20:38 -0700

    Initial Commit

---
+++ ParkPal/ParkPalApp.swift
@@ -0,0 +1,17 @@
+//
+//  ParkPalApp.swift
+//  ParkPal
+//
+//  Created by Shavar Dawkins on 8/29/24.
+//
+
+import SwiftUI
+
+@main
+struct ParkPalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
